68ae78aee2b27940f943087216f225b9c5614e93
nom3wz <nom3wz@bosch.com>
2025-01-16 14:17:34 +0100

    f

--- main.py
+++ main.py
@@ -1,59 +1,147 @@
+"""
+author: Maciej Nowicki
+Date: january 2025
+"""
+
 # Put the code for your API here.
-"""
-Author: Maciej Nowicki
-Date: January, 2025
-script includes fastapi and routing functions
-"""
+from fastapi import FastAPI, HTTPException
+from typing import Union, Optional
 
-import os
-import yaml
-import joblib
-import numpy as np
+# BaseModel from Pydantic is used to define data objects
+from pydantic import BaseModel
 import pandas as pd
-from fastapi import FastAPI, Body
+import os
+import pickle
+from ml.data import process_data
 
-from config import MODEL_DIR, EXAMPLES_DIR
-from app.schemas import Person, FeatureInfo
+# path to saved artifacts
+savepath = "./model"
+filename = ["trained_model.pkl", "encoder.pkl", "labelizer.pkl"]
 
 
-if "DYNO" in os.environ and os.path.isdir("../.dvc"):
-    os.system("dvc config core.no_scm true")
-    if os.system("dvc pull") != 0:
-        exit("dvc pull failed")
-    os.system("rm -r ../.dvc ../.apt/usr/lib/dvc")
+# Declare the data object with its components and their type.
+class InputData(BaseModel):
+    age: int
+    workclass: str
+    fnlgt: int
+    education: str
+    education_num: int
+    marital_status: str
+    occupation: str
+    relationship: str
+    race: str
+    sex: str
+    capital_gain: int
+    capital_loss: int
+    hours_per_week: int
+    native_country: str
 
+    class Config:
+        schema_extra = {
+            "example": {
+                "age": 50,
+                "workclass": "Private",
+                "fnlgt": 234721,
+                "education": "Doctorate",
+                "education_num": 16,
+                "marital_status": "Separated",
+                "occupation": "Exec-managerial",
+                "relationship": "Not-in-family",
+                "race": "Black",
+                "sex": "Female",
+                "capital_gain": 0,
+                "capital_loss": 0,
+                "hours_per_week": 50,
+                "native_country": "United-States",
+            }
+        }
+
+
+# instantiate FastAPI app
 app = FastAPI(
-    title="Udacity - Project 3",
-    description="Deploying a ML Model on Heroku with FastAPI",
-    version="0.1",
+    title="Inference API",
+    description="An API that takes a sample and runs an inference",
+    version="1.0.0",
 )
 
-model = joblib.load(MODEL_DIR)
-with open(EXAMPLES_DIR) as fp:
-    examples = yaml.safe_load(fp)
+
+# load model artifacts on startup of the application to reduce latency
+@app.on_event("startup")
+async def startup_event():
+    global model, encoder, lb
+    # if saved model exits, load the model from disk
+    if os.path.isfile(os.path.join(savepath, filename[0])):
+        model = pickle.load(open(os.path.join(savepath, filename[0]), "rb"))
+        encoder = pickle.load(open(os.path.join(savepath, filename[1]), "rb"))
+        lb = pickle.load(open(os.path.join(savepath, filename[2]), "rb"))
 
 
 @app.get("/")
 async def greetings():
-    return "Greetings and salutations everybody"
+    return "Welcome to our model API"
 
 
-@app.get("/feature_info/{feature_name}")
-async def feature_info(feature_name: FeatureInfo):
-    info = examples["features_info"][feature_name]
-    return info
+# This allows sending of data (our InferenceSample) via POST to the API.
+@app.post("/inference/")
+async def ingest_data(inference: InputData):
+    data = {
+        "age": inference.age,
+        "workclass": inference.workclass,
+        "fnlgt": inference.fnlgt,
+        "education": inference.education,
+        "education-num": inference.education_num,
+        "marital-status": inference.marital_status,
+        "occupation": inference.occupation,
+        "relationship": inference.relationship,
+        "race": inference.race,
+        "sex": inference.sex,
+        "capital-gain": inference.capital_gain,
+        "capital-loss": inference.capital_loss,
+        "hours-per-week": inference.hours_per_week,
+        "native-country": inference.native_country,
+    }
 
+    # prepare the sample for inference as a dataframe
+    sample = pd.DataFrame(data, index=[0])
 
-@app.post("/predict/")
-async def predict(person: Person = Body(..., examples=examples["post_examples"])):
-    person = person.dict()
-    features = np.array([person[f] for f in examples["features_info"].keys()]).reshape(
-        1, -1
+    # apply transformation to sample data
+    cat_features = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
+    ]
+
+    # if saved model exits, load the model from disk
+    if os.path.isfile(os.path.join(savepath, filename[0])):
+        model = pickle.load(open(os.path.join(savepath, filename[0]), "rb"))
+        encoder = pickle.load(open(os.path.join(savepath, filename[1]), "rb"))
+        lb = pickle.load(open(os.path.join(savepath, filename[2]), "rb"))
+
+    sample, _, _, _ = process_data(
+        sample,
+        categorical_features=cat_features,
+        training=False,
+        encoder=encoder,
+        lb=lb,
     )
-    df = pd.DataFrame(features, columns=examples["features_info"].keys())
 
-    pred_label = int(model.predict(df))
-    pred_probs = float(model.predict_proba(df)[:, 1])
-    pred = ">50k" if pred_label == 1 else "<=50k"
+    # get model prediction which is a one-dim array like [1]
+    prediction = model.predict(sample)
 
-    return {"label": pred_label, "prob": pred_probs, "salary": pred}
+    # convert prediction to label and add to data output
+    if prediction[0] > 0.5:
+        prediction = ">50K"
+    else:
+        prediction = ("<=50K",)
+    data["prediction"] = prediction
+
+    return data
+
+
+if __name__ == "__main__":
+    pass
